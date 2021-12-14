8782879c1abf34f1ec84095df8e154c42233657a
dernasherbrezon <rodionovamp@mail.ru>
2021-12-14 14:22:08 +0000

    couple more tests

--- test/api/test_ApiHandler.cpp
+++ test/api/test_ApiHandler.cpp
@@ -1,27 +1,52 @@
 #include <ApiHandler.h>
+#include <ArduinoJson.h>
 #include <unity.h>
+
 #include "MockLoRaModule.h"
 
-String VALID_RX_REQUEST = String("{\n")
-	+ "	\"freq\": 433.0,\n"
-	+ "	\"bw\": 10.4,\n"
-	+ "	\"sf\": 9,\n"
-	+ "	\"cr\": 6,\n"
-	+ "	\"syncWord\": 18,\n"
-	+ "	\"preambleLength\": 55,\n"
-	+ "	\"gain\": 0,\n"
-	+ "	\"ldro\": 0\n"
-	+ "}\n";
+MockLoRaModule mock;
+WebServer web(80);
+
+String VALID_RX_REQUEST =
+    "{\"freq\":433.0,\"bw\":10.4,\"sf\":9,\"cr\":6,\"syncWord\":18,\"power\":"
+    "10,\"preambleLength\":55,\"gain\":0,\"ldro\":0}";
+
+String INVALID_RX_REQUEST =
+    "{freq:433.0,\"bw\":10.4,\"sf\":9,\"cr\":6,\"syncWord\":18,\"power\":"
+    "10,\"preambleLength\":55,\"gain\":0,\"ldro\":0}";
+
+void assertStatus(String *actual, const char *status) {
+  StaticJsonDocument<128> json;
+  DeserializationError error = deserializeJson(json, actual);
+  TEST_ASSERT_NULL(error);
+  TEST_ASSERT_EQUAL_STRING(status, json["status"]);
+}
 
 void test_success_start(void) {
-  MockLoRaModule mock;
-  WebServer web(80);
-
   ApiHandler handler(&web, &mock, NULL, NULL);
   mock.beginCode = 0;
   String output;
   int code = handler.handleStart(VALID_RX_REQUEST, &output);
   TEST_ASSERT_EQUAL_INT(200, code);
+  assertStatus(&output, "SUCCESS");
+}
+
+void test_no_request(void) {
+  ApiHandler handler(&web, &mock, NULL, NULL);
+  mock.beginCode = 0;
+  String output;
+  int code = handler.handleStart((String)NULL, &output);
+  TEST_ASSERT_EQUAL_INT(200, code);
+  assertStatus(&output, "FAILURE");
+}
+
+void test_invalid_json(void) {
+  ApiHandler handler(&web, &mock, NULL, NULL);
+  mock.beginCode = 0;
+  String output;
+  int code = handler.handleStart(INVALID_RX_REQUEST, &output);
+  TEST_ASSERT_EQUAL_INT(200, code);
+  assertStatus(&output, "FAILURE");
 }
 
 void setup() {
@@ -31,6 +56,8 @@ void setup() {
 
   UNITY_BEGIN();
   RUN_TEST(test_success_start);
+  RUN_TEST(test_no_request);
+  RUN_TEST(test_invalid_json);
   UNITY_END();
 }
 

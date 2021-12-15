fd3aa67583b0c2400281f7bb5803389d47ff796d
dernasherbrezon <rodionovamp@mail.ru>
2021-12-15 19:40:12 +0000

    fix test

--- test/embedded/test_LoraModule.cpp
+++ test/embedded/test_LoraModule.cpp
@@ -3,15 +3,15 @@
 
 void test_start_loop_stop(void) {
   LoRaModule module;
-  int code = module.setup(Chip("SX1276 - 868/915Mhz", ChipType::TYPE_SX1276));
+  int code = module.init(Chip("SX1276 - 868/915Mhz", ChipType::TYPE_SX1276));
   TEST_ASSERT_EQUAL(0, code);
-  code = module.begin(915.0, 125.0, (uint8_t)9U, (uint8_t)7U, (uint8_t)18U,
+  code = module.startRx(915.0, 125.0, (uint8_t)9U, (uint8_t)7U, (uint8_t)18U,
                       (uint16_t)8U, (uint8_t)0U, 0);
   TEST_ASSERT_EQUAL(0, code);
   TEST_ASSERT_EQUAL(true, module.isReceivingData());
   // unlikely any message received
   TEST_ASSERT_EQUAL(NULL, module.loop());
-  module.end();
+  module.stopRx();
 }
 
 void setup() {

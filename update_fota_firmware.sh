0a6210d33c6929bb96035d0a4f285a1c51efb160
dernasherbrezon <rodionovamp@mail.ru>
2021-12-23 11:10:24 +0000

    add firmware for tests

---
+++ update_fota_firmware.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+pio test -f testfirmware --without-testing --without-uploading
+
+find .pio/build -name "*/firmware.bin" -print0 | while read -d $'\0' file
+do
+    newname=$(echo ${file} | cut -d '/' -f 3)
+    mv ${file} ./test/resources/fota/${newname}-1.1.bin
+done
